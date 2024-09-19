3ccae7673412d3c7f02c66c07013c60cf2ae9258
Clément Collin <clement.collin@laroueverte.com>
2024-09-19 09:04:38 +0200

    Initial commit

---
+++ src/main/java/org/example/Foo.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class Foo
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
